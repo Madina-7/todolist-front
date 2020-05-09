e12cb92047d9ba794cbf62d85c239b04ada66a36
Your Name <you@example.com>
2020-05-09 19:14:51 +0200

    create update function and replace ID

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient,HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 //describe the class and after declare it 
 @Component({
@@ -8,68 +8,81 @@ import { HttpClient,HttpParams } from '@angular/common/http';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-  todos: any 
+  todos: any
   test: string[]
-  new_item: string='';
+  new_item: string = '';
 
   constructor(private http: HttpClient) {
 
     this.http.get('/api/task').subscribe(
+      data => {
+        console.log(data)
+        this.todos = data;
+      },
+      error => {
+        console.error('There was an error!', error)
+      }
+    )
+  }
+
+
+  ngOnInit(): void { }
+
+  addItem() {
+    if (this.new_item) {
+
+      let newitem = {
+        'title': this.new_item,
+        'completed': false
+      }
+      this.http.post('/api/task', newitem).subscribe(
         data => {
-          console.log(data)
-          this.todos = data;
+          // retrieve updated list 
+          this.http.get('/api/task').subscribe(
+            data => {
+              console.log(data)
+              // send to html by replacing content of this.todos
+              this.todos = data;
+            },
+            error => {
+              console.error('There was an error!', error)
+            }
+          )
         },
         error => {
           console.error('There was an error!', error)
         }
-    )
-   }
+      )
+    }
+  }
 
-
-  ngOnInit(): void {}
-
-  addItem() {
-    if(this.new_item){
-
-      let newitem = {
-        'title': this.new_item,
-        'completed': false,
-        'id': 4674
-      }    
-
-        this.http.post('/api/task',newitem).subscribe(
+  updateItem(item) {
+    this.http.put('/api/task',item).subscribe(
+      data => {
+        // retrieve updated list 
+        this.http.get('/api/task').subscribe(
           data => {
-            console.log('toto');
-            // retrieve updated list 
-            this.http.get('/api/task').subscribe(
-              data => {
-                console.log(data)
-                // send to html by replacing content of this.todos
-                this.todos = data;
-              },
-              error => {
-                console.error('There was an error!', error)
-              }
-            )
+            console.log(data)
+            // send to html by replacing content of this.todos
+            this.todos = data;
           },
           error => {
             console.error('There was an error!', error)
           }
-
-
-
         )
-
-
-    }
+      },
+      error => {
+        console.error('There was an error!', error)
+      }
+    )
   }
 
-  removeItem(todo){
+  removeItem(todo) {
     // send id of task that needs to be deleted as query param
-    let httpParams = new HttpParams().set('taskid', todo.id);
+    let httpParams = new HttpParams().set('taskid', todo._id);
     let options = { params: httpParams };
 
-    this.http.delete('/api/task',options).subscribe(
+    this.http.delete('/api/task', options).subscribe(
       data => {
         // retrieve updated list 
         this.http.get('/api/task').subscribe(
